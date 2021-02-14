f4c8fa16ef3309fde8a996fc41890a6cc38c0b8c
antoniojh10 <antoniohernandez95@gmail.com>
2021-02-14 02:17:35 -0400

    feat: :sparkles: SignOut in header connected to Firebase with useAuth

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Link } from "@reach/router";
+import { useAuth } from "@/hooks/useAuth";
 
 function NavMenu() {
+  const { user, signout } = useAuth();
   return (
     <nav className="navMenu">
       <Link to="/">Home</Link>
-      <Link to="/signin">Sign In</Link>
-      <Link to="/signup">Sign Up</Link>
+      {user ? (
+        <a onClick={signout}>Sign Out</a>
+      ) : (
+        <>
+          <Link to="/signin">Sign In</Link>
+          <Link to="/signup">Sign Up</Link>
+        </>
+      )}
     </nav>
   );
 }
