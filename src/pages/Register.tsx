9a004f2ba0a0f919c13af31ab2dbcf3e236c64d1
antoniojh10 <antoniohernandez95@gmail.com>
2021-02-14 00:07:24 -0400

    test: :white_check_mark: RegisterComponent tests

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
-import RegisterComponent from "@/components/RegisterComponent";
+import RegisterComponent, {
+  RegisterInput
+} from "@/components/RegisterComponent";
 
 function Register(_props: RouteComponentProps) {
-  return <RegisterComponent />;
+  const register = (data: RegisterInput) => {
+    console.log(data);
+  };
+  return <RegisterComponent sendRegisterData={register} />;
 }
 
 export default Register;
