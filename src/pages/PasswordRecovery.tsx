51d912ae99e4f3af6ff3ac94549df625ab0297ef
antoniojh10 <antoniohernandez95@gmail.com>
2021-02-14 01:25:37 -0400

    feat: :sparkles: Not Found and PasswordRecovery pages basic setup

---
+++ src/pages/PasswordRecovery.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+
+function PasswordRecovery(_props: RouteComponentProps) {
+  return <h1>PasswordRecovery</h1>;
+}
+
+export default PasswordRecovery;
