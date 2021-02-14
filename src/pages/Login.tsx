049bed0426a6a17002d8ac25c1d4f39ea090db4e
antoniojh10 <antoniohernandez95@gmail.com>
2021-02-14 02:16:54 -0400

    feat: :sparkles: Login connected to Firebase with useAuth

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
+import { useAuth } from "@/hooks/useAuth";
 import LoginComponent, { LoginInput } from "@/components/LoginComponent";
 
 function Login(_props: RouteComponentProps) {
+  const { signin } = useAuth();
   const login = (data: LoginInput) => {
     console.log(data);
+    signin(data.email, data.password);
   };
   return <LoginComponent sendLoginData={login} />;
 }
